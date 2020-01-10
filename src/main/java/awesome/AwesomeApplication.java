596a4e0f92dd56c7e57a49a2672761d6eb57eff6
yushengmin <yushengmin@oak-inc.com>
2020-01-10 10:44:57 +0800

    Initial commit

---
+++ src/main/java/awesome/AwesomeApplication.java
@@ -0,0 +1,13 @@
+package awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/awesome/AwesomeApplicationTests.java
@@ -0,0 +1,13 @@
+package awesome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
