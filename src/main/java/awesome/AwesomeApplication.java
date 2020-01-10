1d22181bbbc354ec26056ce51ad724d62f8b6a9b
yushengmin <123227320@qq.com>
2020-01-10 17:21:52 +0800

    整合mybatis-plus

--- src/main/java/awesome/AwesomeApplication.java
+++ src/main/java/awesome/AwesomeApplication.java
@@ -1,9 +1,11 @@
 package awesome;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("awesome")
 public class AwesomeApplication {
 
     public static void main(String[] args) {
